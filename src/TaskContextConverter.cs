fa7f794815af39ca121db05eacdf0f29fab6454d
Paul Harrington <Paul.Harrington@outlook.com>
2014-01-19 04:59:07 +0000

    Update ComponentDiagnostics' Task Scheduler view to work with Dev12. Since VsTaskContext is defined in the MPF, it changes from version to version and can't be used in a converter. I switched to using "IConvertible" (which works for all Enums) and then using the values from the interop assembly.
    git-tfs-id: [https://paulharrington.visualstudio.com/defaultcollection]$/Component Diagnostics;C84

--- src/TaskContextConverter.cs
+++ src/TaskContextConverter.cs
@@ -1,24 +1,32 @@
 ﻿using System;
-using Microsoft.VisualStudio.Shell.Interop;
 using Microsoft.VisualStudio.PlatformUI;
-using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
 
 namespace Microsoft.VisualStudio.ComponentDiagnostics
 {
-    sealed class TaskContextConverter : ValueConverter<VsTaskRunContext, string>
+    sealed class TaskContextConverter : ValueConverter<IConvertible, string>
     {
-        protected override string Convert(VsTaskRunContext value, object parameter, System.Globalization.CultureInfo culture)
+        protected override string Convert(IConvertible value, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return Convert((__VSTASKRUNCONTEXT)value.ToUInt32(culture));
+        }
+
+        private static string Convert(__VSTASKRUNCONTEXT value)
         {
             switch (value)
             {
-                case VsTaskRunContext.UIThreadSend:
+                case __VSTASKRUNCONTEXT.VSTC_UITHREAD_SEND:
                     return "UI Thread - RPC";
-                case VsTaskRunContext.BackgroundThread:
+                case __VSTASKRUNCONTEXT.VSTC_BACKGROUNDTHREAD:
                     return "Background Thread";
-                case VsTaskRunContext.UIThreadBackgroundPriority:
+                case __VSTASKRUNCONTEXT.VSTC_UITHREAD_BACKGROUND_PRIORITY:
                     return "UI Thread - Background";
-                case VsTaskRunContext.UIThreadIdlePriority:
+                case __VSTASKRUNCONTEXT.VSTC_UITHREAD_IDLE_PRIORITY:
                     return "UI Thread - Idle";
+                case __VSTASKRUNCONTEXT.VSTC_UITHREAD_NORMAL_PRIORITY:
+                    return "UI Thread - Normal";
+                case __VSTASKRUNCONTEXT.VSTC_BACKGROUNDTHREAD_LOW_IO_PRIORITY:
+                    return "Background, low I/O";
                 default:
                     return value.ToString();
             }
