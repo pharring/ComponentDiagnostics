4c00e7ddbfd8e01f17b6d4fb8634273c0432fea5
Paul Harrington <Paul.Harrington@outlook.com>
2018-01-12 16:46:04 +0000

    Bump version to 1.13 and reset YMM to zero. Use nuget references for Shell Interop and Shell Immutable Update copyright year
    git-tfs-id: [https://paulharrington.visualstudio.com/defaultcollection]$/Component Diagnostics;C255

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.Versioning;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Paul Harrington")]
 [assembly: AssemblyProduct("Visual Studio Component Diagnostics")]
-[assembly: AssemblyCopyright("© 2014 Paul Harrington")]
+[assembly: AssemblyCopyright("© 2018 Paul Harrington")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]   
 [assembly: ComVisible(false)]     
@@ -30,8 +30,8 @@ using System.Runtime.Versioning;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.12.61105.0")]
-[assembly: AssemblyFileVersion("1.12.61105.0")]
+[assembly: AssemblyVersion("1.13.0112.0")]
+[assembly: AssemblyFileVersion("1.13.0112.0")]
 
 // Declare the component guarantees. This is an assembly with internal API, that is not intended for public use.
 [assembly: ComponentGuarantees(ComponentGuaranteesOptions.None)]
